cb2a534057b121b7d9a3fb41843d4e11a579ea41
Dreampie <wangrenhui1990@gmail.com>
2014-11-20 12:11:14 +0800

    add  minify  test  only  support  jdk  1.7

--- src/test/java/cn/dreampie/CoffeeCompilerTest.java
+++ src/test/java/cn/dreampie/CoffeeCompilerTest.java
@@ -11,6 +11,7 @@ public class CoffeeCompilerTest {
     File input = new File(getClass().getResource("/test.coffee").getPath());
     CoffeeSource coffeeSource = new CoffeeSource(input);
     CoffeeCompiler coffeeCompiler = new CoffeeCompiler();
+    //only  support  jdk1.7
     coffeeCompiler.setCompress(true);
 
     File output = new File(input.getAbsolutePath().replace(".coffee", ".js"));
